d4a7fbb5892d2ec7874943e288cb85ee229922a9
bourne <zx9527@hotmail.com>
2018-06-13 16:29:38 +0800

    RFC adapter V2,call rfc and process return message and send to mq

--- src/main/java/com/fl/integration/sap/rfc/inbound/RfcClientSource.java
+++ src/main/java/com/fl/integration/sap/rfc/inbound/RfcClientSource.java
@@ -3,16 +3,35 @@ package com.fl.integration.sap.rfc.inbound;
 import com.sap.conn.jco.JCoDestination;
 import com.sap.conn.jco.JCoDestinationManager;
 import com.sap.conn.jco.JCoException;
-
+import org.apache.log4j.Logger;
 /**
  * @author hasee
  * @create 2018-06-07 15:28
  **/
 public class RfcClientSource {
 
-	static String ABAP_AS_POOLED = "ABAP_AS_WITH_POOL";
+	private static final Logger logger = Logger.getLogger(RfcClientSource.class);
+	private String ABAP_AS_POOLED = "ABAP_AS_WITH_POOL";
 
-	//private JCoDestination destination;
+	private JCoDestination destination;
+
+	public JCoDestination getDestination() throws JCoException {
+		try {
+			return createDestination();
+		} catch (Exception e) {
+			if (logger.isDebugEnabled()) {
+				logger.info("RfcClientSource error,get JCoDestination error");
+			}
+
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void setDestination(JCoDestination destination) {
+		this.destination = destination;
+	}
+
 	private synchronized JCoDestination createDestination() throws JCoException {
 		JCoDestination destination = JCoDestinationManager.getDestination(ABAP_AS_POOLED);
 		return destination;
